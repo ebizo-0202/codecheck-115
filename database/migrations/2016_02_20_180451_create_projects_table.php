069383cf0ef28c1bc0dc1c6a763ab583b12e9165
icchii0618 <y.icchii.0618@gmail.com>
2016-02-21 03:45:20 +0900

    [add] add migration file of creating project table.

---
+++ database/migrations/2016_02_20_180451_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id')->integer();
+            $table->string('url', 255)->nullable();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}

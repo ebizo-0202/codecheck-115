625c91aee59c0f2d7c9fea3d947bf37a60c518cc
icchii0618 <y.icchii.0618@gmail.com>
2016-02-21 03:48:31 +0900

    [add] routing getting all projects.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('projects', 'ApiController@index');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes

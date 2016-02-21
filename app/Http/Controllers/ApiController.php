3db5e3164c83d3b02f8c566f1552b7e0b9c531d3
icchii0618 <y.icchii.0618@gmail.com>
2016-02-21 21:43:58 +0900

    fix db

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,6 +16,9 @@ class ApiController extends Controller
         $projects = new Project;
         $allProjects = $projects->all();
 
+        if(!$allProjects) {
+            return response()->json([ 'error' => 404, 'message' => 'NotFound' ], 404);
+        }
         return response()->json([ 'status' => 200, 'message' => 'OK', 'results' => $allProjects], 200);
        // return response()->json(200);
     }
@@ -40,7 +43,8 @@ class ApiController extends Controller
             return response()->json([ 'error' => 400, 'message' => 'BadRequest' ], 400);
         }
 
-        $project = Project::create();
+        //$project = Project::create();
+        $project = new Project;
         if (!$request->title || !$request->description){
             return response()->json([ 'error' => 400, 'message' => 'BadRequest' ], 400);
         }
@@ -48,7 +52,7 @@ class ApiController extends Controller
         $project->description = $request->description;
         $project->save();
 
-        return response()->json(200);
+        return response()->json([ 'error' => 200, 'message' => 'OK' ], 200);
     }
     
     public function detail($id)
--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+//  protected $table = 'projects';
+     /**
+     * モデルに関連付けるデータベースのテーブルを指定
+     *
+     * @var string
+     */
+     //protected $table = 'projects';
+
+     /**
+     * createメソッド利用時に、入力を受け付けないカラムの指定
+     *
+     * @var array
+     */
+     //protected $fillable = array('title', 'description'); // この行を追加。
 }
